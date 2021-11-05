9ee70c4b355a70520a9ab0e4efe0fbcb652e80ee
Medha <tyagi.25@gmail.com>
2021-11-05 12:58:30 +0000

    added few algorithms for python

---
+++ binarySearch.py
@@ -0,0 +1,18 @@
+def binary_search_recursive(array, element, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    if element == array[mid]:
+        return mid
+
+    if element < array[mid]:
+        return binary_search_recursive(array, element, start, mid-1)
+    else:
+        return binary_search_recursive(array, element, mid+1, end)
+
+element = 18
+array = [1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29]
+
+print("Searching for {}".format(element))
+print("Index of {}: {}".format(element, binary_search_recursive(array, element, 0, len(array))))
